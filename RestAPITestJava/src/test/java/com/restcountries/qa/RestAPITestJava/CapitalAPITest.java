0ad25032d1b390f06138bf8c5eaeabdb171df08b
Durai4024 <duraivvenkat@gmail.com>
2025-03-19 20:50:25 +0530

    Added rest assured API tests

--- RestAPITestJava/src/test/java/com/restcountries/qa/RestAPITestJava/CapitalAPITest.java
+++ RestAPITestJava/src/test/java/com/restcountries/qa/RestAPITestJava/CapitalAPITest.java
@@ -1,27 +1,34 @@
 package com.restcountries.qa.RestAPITestJava;
 
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
-import io.restassured.response.Response;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class CapitalAPITest {
-	
-  @Test
-  public void getCapitalAPITest() {
-	  RestAssured.baseURI = "https://restcountries.com";
-	  RestAssured.basePath = "v3.1/capital";
-	  
-	  given().when().get("/tallinn")
-	  .then().statusCode(200);
-	  
-	  Response res = given().when().get("/tallinn");
-	  
-	  System.out.println(res.asPrettyString());
-	  
-	  
-	  
-  }
+
+	@BeforeClass
+	public void setup() {
+
+		RestAssured.baseURI = "https://restcountries.com";
+		RestAssured.basePath = "v3.1/capital";
+	}
+
+	@Test
+	public void getCapitalTest() {
+
+		given().when().get("/tallinn").then().statusCode(200).and().body("[0].name.common", equalTo("Estonia")).and()
+				.body("[0].name.official", equalTo("Republic of Estonia"));
+
+	}
+
+	@Test
+	public void getCapitalWithInvalidDataTest() {
+
+		given().when().get("/New York").then().statusCode(404).and().body("message", equalTo("Not Found"));
+
+	}
 }
